4e76c3dc98922447ef5324ac9420e2f770e88034
Łukasz Flisak <lu.dev00@gmail.com>
2025-04-29 16:01:31 +0200

    Add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=150, unique=True)
+    is_instructor = models.BooleanField(default=False)
+    jwt_token = models.TextField(blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
